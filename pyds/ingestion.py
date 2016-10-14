5cf2a0fe168eba020b3d8c19db31537e7d842e9d
TP <talperetz24@gmail.com>
2016-10-14 15:23:36 +0300

    module that receives relational data in various formats and returns a matching pandas dataframe

---
+++ pyds/ingestion.py
@@ -0,0 +1,44 @@
+""" 
+@author: Tal Peretz
+@date: 10/14/2016
+@TL;DR: this module gets relational data in several forms and returns a pandas dataframe
+"""
+
+import pandas as pd
+
+# supported file extensions
+file_extension_to_read_attribute = {'csv': 'read_csv', 'excel': 'read_excel', 'hdf': 'read_hdf', 'sql': 'read_sql',
+                                    'json': 'read_json', 'html': 'read_html', 'stata': 'read_stata', 'sas': 'read_sas',
+                                    'pickle': 'read_pickle'}
+
+
+def get_file_extension(file_path):
+    """
+    given the path of a file
+    returns it's extension as string
+    :param file_path: the path of an input file
+    :return: the extension of the file
+    """
+    if file_path is None:
+        raise ValueError('file path is None')
+    else:
+        file_extension = file_path.split(".")[-1]
+        if file_extension == "":
+            raise ValueError('expected .[extension] file')
+        elif file_extension not in file_extension_to_read_attribute.keys():
+            raise ValueError('supported file types are \n %s' % file_extension_to_read_attribute.keys())
+    return file_extension
+
+
+def read(*args):
+    """
+    given a collection of file paths representing relational data
+    returns a pandas dataframe of the data
+    :param args: collection of file paths representing an input file
+    :return: pandas dataframe
+    """
+    partial_dfs = []
+    for index, file_path in enumerate(args):
+        pd_read_function = getattr(pd, file_extension_to_read_attribute[get_file_extension(file_path)])
+        partial_dfs.append(pd_read_function(file_path))
+    return pd.concat(partial_dfs)
