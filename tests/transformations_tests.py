46f53bc730f8bf4dabbfef08101a52b23327b40a
tal peretz <talperetz24@gmail.com>
2016-12-14 12:22:21 +0200

    implemented tests for transformations module

--- tests/transformations_tests.py
+++ tests/transformations_tests.py
@@ -8,8 +8,10 @@
 
 import os
 import unittest
+import numpy as np
 
 from pyds import transformations, constants
+from tests import data_generators
 
 
 class PipelineTestCase(unittest.TestCase):
@@ -37,24 +39,36 @@ class PipelineTestCase(unittest.TestCase):
             warnings.showwarning = warn_with_traceback
 
     def test_discretize(self):
-        # todo: generate dataframe with numerical values
-        equal_width_num_df, col_to_width_edges, equal_depth_num_df, col_to_depth_edges = transformations.discretize()
-        # todo: check the returning values
-        pass
+        gen_df = data_generators.generate_random_data(100, 5)
+        equal_width_num_df, col_to_width_edges, equal_depth_num_df, col_to_depth_edges = transformations.discretize(
+            gen_df)
+        equal_width_num_df_with_predefined_bins, _, equal_depth_num_df_with_predefined_bins, _ = transformations.discretize(
+            gen_df, col_to_width_edges, col_to_depth_edges)
+
+        # check that equal_width_num_df and equal_depth_num_df has optimal num of bins
+        for col_name in gen_df:
+            self.assertEqual(len(equal_width_num_df["equal_w_%s" % col_name].unique()),
+                             transformations._calc_optimal_num_of_bins(gen_df[col_name]))
+            self.assertEqual(len(equal_depth_num_df["equal_d_%s" % col_name].unique()),
+                             transformations._calc_optimal_num_of_bins(gen_df[col_name]))
+        self.assertTrue(equal_width_num_df_with_predefined_bins.equals(equal_width_num_df))
+        self.assertTrue(equal_depth_num_df_with_predefined_bins.equals(equal_depth_num_df))
 
     def test_preprocess_train_columns(self):
-        # todo: generate dataframes
-        transformed_df, col_to_scaler, col_to_encoder, updated_numerical_cols, updated_categorical_cols, \
-        col_to_width_edges, col_to_depth_edges = transformations.preprocess_train_columns()
-        # todo: check the returning values
-        pass
+        hr_df = data_generators.get_hr_dataset()
+        transformed_df, train_transformations = transformations.preprocess_train_columns(hr_df)
+        self.assertEqual(len(hr_df.select_dtypes(include=[np.number])),
+                         len(transformed_df.select_dtypes(include=[np.number])))  # num of num cols hasn't changed
+        self.assertGreaterEqual(len(hr_df.select_dtypes(include=['category'])),
+                         len(transformed_df.select_dtypes(include=['category'])))  # num of cat cols is greater or equal
 
     def test_preprocess_test_columns(self):
-        # todo: generate dataframes
-        transformed_df = transformations.preprocess_test_columns()
-        # todo: check the returning values
-        pass
-
+        # run preprocess_train_columns and preprocess_test_columns on same raw data and check if the returned dataframes
+        # are equal
+        hr_df = data_generators.get_hr_dataset()
+        transformed_train_df, train_transformations = transformations.preprocess_train_columns(hr_df)
+        transformed_test_df = transformations.preprocess_test_columns(hr_df, train_transformations)
+        self.assertTrue(transformed_train_df.equals(transformed_test_df))
 
 if __name__ == '__main__':
     unittest.main()
